a15c89d82e8e906bd951b07109fceec9d2f6cdf9
PashaDev <86747342+PashaDEVAcc@users.noreply.github.com>
2022-10-04 19:37:21 -0700

    Update ContentView.swift
    those variables aren't needed

--- Job App/Views/ContentView.swift
+++ Job App/Views/ContentView.swift
@@ -2,10 +2,6 @@
 import SwiftUI
 
 struct ContentView: View {
-    
-    @State var isLoginMode = false
-    @State var email = ""
-    @State var password = ""
     var body: some View {
         NavigationView {
             LoginCreateAccountView()
